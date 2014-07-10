f8067565c782b8daa81205bcfd01b39b83fefc41
Wayne Gaudin <wpg@pan.hpsg.dcs.warwick.ac.uk>
2014-07-10 12:09:42 +0100

    Q fixes

--- viscosity_kernel.f90
+++ viscosity_kernel.f90
@@ -54,29 +54,51 @@ SUBROUTINE viscosity_kernel(x_min,x_max,y_min,y_max,z_min,z_max,    &
   REAL(KIND=8), DIMENSION(x_min-2:x_max+3,y_min-2:y_max+3,z_min-2:z_max+3)     :: xvel0,yvel0,zvel0
 
   INTEGER       :: j,k,l
-  REAL(KIND=8)  :: ugrad,vgrad,wgrad,grad2,pgradx,pgrady,pgradz,pgradx2,pgrady2,pgradz2,grad     &
-                  ,ygrad,pgrad,xgrad,zgrad,div,strain2,limiter
+  REAL(KIND=8)  :: ugradx1,ugradx2,vgrady1,vgrady2,wgradz1,wgradz2,xx,yy,zz,xy,xz,yz
+  REAL(KIND=8)  :: ugrady1,ugrady2,vgradx1,vgradx2,wgradx1,wgradx2
+  REAL(KIND=8)  :: ugradz1,ugradz2,vgradz1,vgradz2,wgrady1,wgrady2
+  REAL(KIND=8)  :: grad2,pgradx,pgrady,pgradz,pgradx2,pgrady2,pgradz2,grad     &
+                  ,ygrad,pgrad,xgrad,zgrad,div,limiter
 
 !$OMP PARALLEL
 
-!$OMP DO PRIVATE(ugrad,vgrad,wgrad,div,strain2,pgradx,pgrady,pgradz,pgradx2,pgrady2,pgradz2,limiter,pgrad,xgrad,ygrad,zgrad,grad,grad2)
+!$OMP DO PRIVATE(ugradx1,ugradx2,vgrady1,vgrady2,wgradz1,wgradz2,div,                     &
+!$OMP            ugrady1,ugrady2,vgradx1,vgradx2,wgradx1,wgradx2,                         &
+!$OMP            ugradz1,ugradz2,vgradz1,vgradz2,wgrady1,wgrady2,                         &
+!$OMP            pgradx,pgrady,pgradz,pgradx2,pgrady2,pgradz2,limiter,                    &
+!$OMP            pgrad,xgrad,ygrad,zgrad,grad,grad2,xx,yy,zz,xy,xz,yz)
   DO l=z_min,z_max
     DO k=y_min,y_max
       DO j=x_min,x_max
-        ugrad=0.5_8*((xvel0(j+1,k  ,l  )+xvel0(j+1,k+1,l  )+xvel0(j+1,k  ,l+1)+xvel0(j+1,k+1,l+1)) &
-                    -(xvel0(j  ,k  ,l  )+xvel0(j  ,k+1,l  )+xvel0(j  ,k  ,l+1)+xvel0(j  ,k+1,l+1)))
+        ugradx1=xvel0(j  ,k  ,l  )+xvel0(j  ,k+1,l  )+xvel0(j  ,k  ,l+1)+xvel0(j  ,k+1,l+1)
+        ugradx2=xvel0(j+1,k  ,l  )+xvel0(j+1,k+1,l  )+xvel0(j+1,k  ,l+1)+xvel0(j+1,k+1,l+1)
+        ugrady1=xvel0(j  ,k  ,l  )+xvel0(j+1,k  ,l  )+xvel0(j  ,k  ,l+1)+xvel0(j+1,k  ,l+1)
+        ugrady2=xvel0(j  ,k+1,l  )+xvel0(j+1,k+1,l  )+xvel0(j  ,k+1,l+1)+xvel0(j+1,k+1,l+1)
+        ugradz1=xvel0(j  ,k  ,l  )+xvel0(j+1,k  ,l  )+xvel0(j  ,k+1,l  )+xvel0(j+1,k+1,l  )
+        ugradz2=xvel0(j  ,k  ,l+1)+xvel0(j+1,k  ,l+1)+xvel0(j  ,k+1,l+1)+xvel0(j+1,k+1,l+1)
 
-        vgrad=0.5_8*((yvel0(j  ,k+1,l  )+yvel0(j+1,k+1,l  )+yvel0(j  ,k+1,l+1)+yvel0(j+1,k+1,l+1)) &
-                    -(yvel0(j  ,k  ,l  )+yvel0(j+1,k  ,l  )+yvel0(j  ,k  ,l+1)+yvel0(j+1,k  ,l+1)))
+        vgradx1=yvel0(j  ,k  ,l  )+yvel0(j  ,k+1,l  )+yvel0(j  ,k  ,l+1)+yvel0(j  ,k+1,l+1)
+        vgradx2=yvel0(j+1,k  ,l  )+yvel0(j+1,k+1,l  )+yvel0(j+1,k  ,l+1)+yvel0(j+1,k+1,l+1)
+        vgrady1=yvel0(j  ,k  ,l  )+yvel0(j+1,k  ,l  )+yvel0(j  ,k  ,l+1)+yvel0(j+1,k  ,l+1)
+        vgrady2=yvel0(j  ,k+1,l  )+yvel0(j+1,k+1,l  )+yvel0(j  ,k+1,l+1)+yvel0(j+1,k+1,l+1)
+        vgradz1=yvel0(j  ,k  ,l  )+yvel0(j+1,k  ,l  )+yvel0(j  ,k+1,l  )+yvel0(j+1,k+1,l  )
+        vgradz2=yvel0(j  ,k  ,l+1)+yvel0(j+1,k  ,l+1)+yvel0(j  ,k+1,l+1)+yvel0(j+1,k+1,l+1)
 
-        wgrad=0.5_8*((zvel0(j  ,k  ,l+1)+zvel0(j+1,k+1,l+1)+zvel0(j  ,k  ,l+1)+zvel0(j+1,k+1,l+1)) &
-                    -(zvel0(j  ,k  ,l  )+zvel0(j+1,k  ,l  )+zvel0(j  ,k+1,l  )+zvel0(j+1,k+1,l  )))
+        wgradx1=zvel0(j  ,k  ,l  )+zvel0(j  ,k+1,l  )+zvel0(j  ,k  ,l+1)+zvel0(j  ,k+1,l+1)
+        wgradx2=zvel0(j+1,k  ,l  )+zvel0(j+1,k+1,l  )+zvel0(j+1,k  ,l+1)+zvel0(j+1,k+1,l+1)
+        wgrady1=zvel0(j  ,k  ,l  )+zvel0(j+1,k  ,l  )+zvel0(j+1,k  ,l+1)+zvel0(j+1,k  ,l+1)
+        wgrady2=zvel0(j  ,k+1,l  )+zvel0(j+1,k+1,l  )+zvel0(j+1,k+1,l+1)+zvel0(j+1,k+1,l+1)
+        wgradz1=zvel0(j  ,k  ,l  )+zvel0(j+1,k  ,l  )+zvel0(j  ,k+1,l  )+zvel0(j+1,k+1,l  )
+        wgradz2=zvel0(j  ,k  ,l+1)+zvel0(j+1,k  ,l+1)+zvel0(j+1,k  ,l+1)+zvel0(j+1,k+1,l+1)
 
-        div = (xarea(j,k,l)*(ugrad)+  yarea(j,k,l)*(vgrad))+ zarea(j,k,l)*(wgrad)
+        div = (xarea(j,k,l)*(ugradx2-ugradx1)+  yarea(j,k,l)*(vgrady2-vgrady1))+ zarea(j,k,l)*(wgradz2-wgradz1)
 
-        strain2 = 0.5_8*(xvel0(j,  k+1,l  ) + xvel0(j+1,k+1,l+1)-xvel0(j  ,k  ,l)-xvel0(j+1,k  ,l  ))/xarea(j,k,l) &
-                + 0.5_8*(yvel0(j+1,k  ,l  ) + yvel0(j+1,k+1,l+1)-yvel0(j  ,k  ,l)-yvel0(j  ,k+1,l  ))/yarea(j,k,l) &
-                + 0.5_8*(zvel0(j  ,k  ,l+1) + zvel0(j+1,k+1,l+1)-zvel0(j  ,k  ,l)-zvel0(j  ,k  ,l+1))/zarea(j,k,l)
+        xx = 0.25_8*(ugradx2-ugradx1)/(celldx(j))
+        yy = 0.25_8*(vgrady2-vgrady1)/(celldy(k))
+        zz = 0.25_8*(wgradz2-wgradz1)/(celldz(l))
+        xy = 0.25_8*(ugrady2-ugrady1)/(celldy(k))+0.25_8*(vgradx2-vgradx1)/(celldx(j))
+        xz = 0.25_8*(ugradz2-ugradz1)/(celldz(l))+0.25_8*(wgradx2-wgradx1)/(celldx(j))
+        yz = 0.25_8*(vgradz2-vgradz1)/(celldz(l))+0.25_8*(wgrady2-wgrady1)/(celldy(j))
 
         pgradx=(pressure(j+1,k,l)-pressure(j-1,k,l))/(celldx(j)+celldx(j+1))
         pgrady=(pressure(j,k+1,l)-pressure(j,k-1,l))/(celldy(k)+celldy(k+1))
@@ -86,9 +108,9 @@ SUBROUTINE viscosity_kernel(x_min,x_max,y_min,y_max,z_min,z_max,    &
         pgrady2 = pgrady*pgrady
         pgradz2 = pgradz*pgradz
 
-        limiter = ((0.5_8*(ugrad)/celldx(j))*pgradx2+(0.5_8*(vgrad)/celldy(k))*pgrady2 &
-                +(0.5_8*(wgrad)/celldz(l))*pgradz2+strain2*pgradx*pgrady*pgradz)  &
-                /MAX(pgradx2+pgrady2+pgradz2,1.0e-16_8)
+        limiter = (xx*pgradx2+yy*pgrady2+zz*pgradz2                    &
+                +  xy*pgradx*pgrady+xz*pgradx*pgradz+yz*pgrady*pgradz) &
+                / MAX(pgradx2+pgrady2+pgradz2,1.0e-16_8)
 
         IF ((limiter.GT.0.0).OR.(div.GE.0.0))THEN
           viscosity(j,k,l) = 0.0
